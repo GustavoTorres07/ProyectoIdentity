d87a94ed5bb7512f0e135f4816891d61c626d0b1
Gustavo Torres <gtreal62@gmail.com>
2022-10-14 14:10:54 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoIdentity/Controllers/CuentasController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoIdentity.Models;
+
+namespace ProyectoIdentity.Controllers
+{
+    public class CuentasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Registro()
+        {
+
+            RegistroViewModel regisitroVM = new RegistroViewModel();
+            return View(regisitroVM);
+
+        }
+
+
+    }
+}
---
+++ ProyectoIdentity/Datos/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProyectoIdentity.Models;
+
+namespace ProyectoIdentity.Datos
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Agrego los diferentes modelos que necesito (si no se agregan los modelos la migracion va a ser vacia).
+        public DbSet<AppUsuario> AppUsuario { get; set; }   
+
+    }
+
+}
